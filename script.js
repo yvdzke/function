9cf435e8e25e3dfa0914d9099d7ac0d6be8046f0
yvdzke <yudzke@gmail.com>
2025-09-19 19:51:03 +0700

    membuat function di file script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function jumlahVolumeDuaKubus(a, b) {
+  // a dan b adalah sisi dari kubus
+  var volumeA = a * a * a;
+  var volumeB = b * b * b;
+  var totalVolume = volumeA + volumeB;
+  return totalVolume;
+}
+console.log(jumlahVolumeDuaKubus(8, 3)); // 539
